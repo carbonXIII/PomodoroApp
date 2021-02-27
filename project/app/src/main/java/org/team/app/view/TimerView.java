7621d8b370f2541e9c528a43826c9e8b442da63f
Jordan Richards <r1ch4rds.j0rdan@gmail.com>
2021-02-26 20:51:24 -0500

    updated mock timer screen, other screens have not been updated yet

--- project/app/src/main/java/org/team/app/view/TimerView.java
+++ project/app/src/main/java/org/team/app/view/TimerView.java
@@ -1,12 +1,10 @@
 package org.team.app.view;
 
 import android.os.Bundle;
-import androidx.fragment.app.Fragment;
 import android.widget.Button;
 import android.widget.TextView;
 import android.view.View;
 
-import org.team.app.view.R;
 import org.team.app.contract.TimerContract;
 
 public class TimerView extends FragmentView implements TimerContract.View {
@@ -25,7 +23,7 @@ public class TimerView extends FragmentView implements TimerContract.View {
     public void setTitle(String title, String taskName) {
         View view = getView();
 
-        final TextView titleText = view.findViewById(R.id.text_timer_title);
+        final TextView titleText = view.findViewById(R.id.text_task_title);
         titleText.setText(title);
 
         final TextView taskNameText = view.findViewById(R.id.text_task_name);
@@ -48,7 +46,7 @@ public class TimerView extends FragmentView implements TimerContract.View {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        final Button button = view.findViewById(R.id.button2);
+        final Button button = view.findViewById(R.id.button_finish);
         button.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
                     mPresenter.onTimerComplete();
