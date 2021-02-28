60c25cb2eefc5136e8779e272ab060c8d8e8d4fd
ishaanb555 <78178070+ishaanb555@users.noreply.github.com>
2021-02-28 18:05:25 -0500

    Update Task.java
    Comments on the SOLID PRINCIPLES
    // Single responsibility: Store class data
    //open/closed: Easily modified easily by extending class and adding more getters, like time in focus and on break
    //Liskov substitution: no child classes as of yet but we will make sure they are replaceable
    //Interface segregation: no interfaces used but we will avoid repetition
    //Dependency Inversion: No dependencies in this piece, minus a import, further discussed in build.gradle

--- project/app/src/main/java/org/team/app/model/Task.java
+++ project/app/src/main/java/org/team/app/model/Task.java
@@ -1,20 +1,20 @@
 package org.team.app.model;
 
 import java.util.UUID;
-
+//class based on Tasks storage
 public class Task {
     protected final UUID uuid;
     public final String name;
-
+    
     public Task(String name) {
         uuid = UUID.randomUUID();
         this.name = name;
     }
-
+    
     public final String getName() {
         return this.name;
     }
-
+    
     public final UUID getUUID() {
         return this.uuid;
     }
