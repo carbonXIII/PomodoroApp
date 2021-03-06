e13e4072d4808ad12f345f25dcc65b6a558321d9
ishaanb555 <78178070+ishaanb555@users.noreply.github.com>
2021-03-06 15:43:58 -0500

    Java file for the login screen
    Login screen Java file, used as the new activity screen

---
+++ project/app/src/main/java/org/team/app/view/login_screen.java
@@ -0,0 +1,14 @@
+package org.team.app.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class login_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_screen);
+    }
+}
