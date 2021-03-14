33b3a57c859dcafd426187c83499077c72c45c81
KimberlySzekely <kimszekely319@knights.ucf.edu>
2021-03-14 19:18:38 -0400

    creates framework for sound playing tool and adds sample sound file

---
+++ project/app/src/main/java/org/team/app/view/AudioPlayer.java
@@ -0,0 +1,19 @@
+package org.team.app.view;
+
+public class AudioPlayer {
+
+    // Constructor to utilize MediaPlayer class for any audio that might be needed
+    AudioPlayer(){
+
+    }
+
+    // Method to play passed sound
+
+    // Method to stop playing the sound before the sound completes
+
+    // Method to change volume of sound playing
+
+    // Method to play the sound on loop
+
+
+}
